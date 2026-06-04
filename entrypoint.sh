fcdb5a38885deae59e22f9eee5a3152a93658ffa
4damAce <65834741+4damAce@users.noreply.github.com>
2026-06-04 04:35:07 +0100

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-# Generate nginx config with environment variables substituted
-envsubst '$ODYSSEUS_PC_HOST,$ODYSSEUS_PC_PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-exec "$@"
